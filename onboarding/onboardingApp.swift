9db75128abebcf812013643bb922c480cd06aa75
Ajmal Afif <ajmalafif@gmail.com>
2025-03-17 23:53:03 +0800

    Initial Commit

---
+++ onboarding/onboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  onboardingApp.swift
+//  onboarding
+//
+//  Created by Ajmal Afif  on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct onboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
